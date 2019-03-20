a9ba425884af27a2ffc43ff2eeff6f646ce33de6
Minh Pham <mpham@naver.com>
2019-03-20 19:29:19 +0100

    Completed main authentication function

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -13,9 +26,20 @@ passport.use(
     //   console.log(accessToken);
     // }
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile", profile);
+      // console.log("access token", accessToken);
+      // console.log("refreshToken", refreshToken);
+      // console.log("profile", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({
+            googleId: profile.id
+          })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
