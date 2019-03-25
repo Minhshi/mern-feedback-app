ab70f597cea36ede3c7097f76826966e81430e56
Minh Pham <mpham@naver.com>
2019-03-25 14:16:49 +0100

    Add billing routes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 
 const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+
 const keys = require("./config/keys");
 
 // const passport = require("passport");
@@ -15,6 +18,8 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -58,6 +63,7 @@ app.use(passport.session());
 // app.get("/auth/google/callback", passport.authenticate("google"));
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ routes/billingRoutes.js
@@ -0,0 +1,18 @@
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
+
+module.exports = app => {
+  // app.post("/api/stripe", (req, res) => {
+  //   console.log(req.body)
+  // })
+  app.post("/api/stripe", async (req, res) => {
+    // console.log(req.body)
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      description: "$5 for 5 Feedback Credits",
+      source: req.body.id
+    });
+    console.log(charge);
+  });
+};
