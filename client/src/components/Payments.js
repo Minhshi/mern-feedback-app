221382ec22c26c0ea8499312ff9b337b1cea337d
Minh Pham <mpham@naver.com>
2019-03-25 13:11:27 +0100

    Wire handleToken action to Payments component

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import { fetchUser, handleToken } from "../actions";
 
 class Payments extends React.Component {
   render() {
@@ -8,13 +10,16 @@ class Payments extends React.Component {
         name="Feedback App"
         description="$5 for 5 Feedback Credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
-      <button className="btn">Add Credits</button>
+        <button className="btn">Add Credits</button>
       </StripeCheckout>
     );
   }
 }
 
-export default Payments;
+export default connect(
+  null,
+  { fetchUser, handleToken }
+)(Payments);
