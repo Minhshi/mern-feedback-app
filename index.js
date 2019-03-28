463a77d014ac4b6675031f313ad18cafc5cb1199
Minh Pham <mpham@naver.com>
2019-03-28 17:08:57 +0100

    Add routes for production

--- index.js
+++ index.js
@@ -65,5 +65,14 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(expresss.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
