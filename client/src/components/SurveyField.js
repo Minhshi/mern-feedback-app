602448cf223c47d0379f97b00467446fc90ca00a
Minh Pham <mpham@naver.com>
2019-04-23 13:55:10 +0200

    Add all SurveyFields

--- client/src/components/SurveyField.js
+++ client/src/components/SurveyField.js
@@ -5,6 +5,7 @@ class SurveyField extends React.Component {
     // console.log(this.props);
     return (
       <div>
+        <label>{this.props.label}</label>
         <input {...this.props.input} />
       </div>
     );
--- client/src/components/SurveyForm.js
+++ client/src/components/SurveyForm.js
@@ -1,37 +1,55 @@
 import React from "react";
 import { reduxForm, Field } from "redux-form";
-import SurveyField from "./SurveyField"
+import SurveyField from "./SurveyField";
 
 class SurveyForm extends React.Component {
   renderFields() {
     return (
       <div>
         <Field
-        type="text"
-        name="title"
-        component={SurveyField}
+          label="Survey Title"
+          type="text"
+          name="title"
+          component={SurveyField}
+        />
+        <Field
+          label="Subject Line"
+          type="text"
+          name="subject"
+          component={SurveyField}
+        />
+        <Field
+          label="Email Body"
+          type="text"
+          name="body"
+          component={SurveyField}
+        />
+        <Field
+          label="Recipient List"
+          type="text"
+          name="emails"
+          component={SurveyField}
         />
       </div>
-      )
+    );
   }
 
   render() {
     return (
       <div>
-      <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-
-      {/*<Field
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {/*<Field
         type="text"
         name="surveyTitle"
         component="input"
       />*/}
 
-      {this.renderFields()}
+          {this.renderFields()}
 
-      <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
       </div>
-      )
+    );
   }
 }
 
