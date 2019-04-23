ba13b7bf6219700b9a9476544dba9b3dd11ddbba
Minh Pham <mpham@naver.com>
2019-04-23 14:11:20 +0200

    Refactor renderFields

--- client/src/components/SurveyForm.js
+++ client/src/components/SurveyForm.js
@@ -2,36 +2,26 @@ import React from "react";
 import { reduxForm, Field } from "redux-form";
 import SurveyField from "./SurveyField";
 
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails" }
+];
+
 class SurveyForm extends React.Component {
   renderFields() {
-    return (
-      <div>
-        <Field
-          label="Survey Title"
-          type="text"
-          name="title"
-          component={SurveyField}
-        />
-        <Field
-          label="Subject Line"
-          type="text"
-          name="subject"
-          component={SurveyField}
-        />
-        <Field
-          label="Email Body"
-          type="text"
-          name="body"
-          component={SurveyField}
-        />
-        <Field
-          label="Recipient List"
-          type="text"
-          name="emails"
-          component={SurveyField}
-        />
-      </div>
-    );
+    return FIELDS.map(field => {
+      return (
+        <div key={field.name}>
+          <Field
+            component={SurveyField}
+            label={field.label}
+            name={field.name}
+          />
+        </div>
+      );
+    });
   }
 
   render() {
