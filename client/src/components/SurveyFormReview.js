7f6436edd3b7b4a22b220f5ff0da689d2ef7e71d
Minh Pham <mpham@naver.com>
2019-04-25 14:13:17 +0200

    Add styles to buttons in SurveyFormReview

--- client/src/components/SurveyFormReview.js
+++ client/src/components/SurveyFormReview.js
@@ -6,9 +6,11 @@ class SurveyFormReview extends React.Component {
   reviewFields() {
     return FIELDS.map(field => {
       return (
-        <div>
-          <label>{field.label}</label>
-          <div>{this.props.formValues[field.name]}</div>
+        <div key={field.name}>
+          <div>
+            <label>{field.label}</label>
+            <div>{this.props.formValues[field.name]}</div>
+          </div>
         </div>
       );
     });
@@ -20,10 +22,14 @@ class SurveyFormReview extends React.Component {
         <h5>Please confirm your entries</h5>
         {this.reviewFields()}
         <button
-          className="yellow darken-3 btn-flat"
+          className="yellow darken-3 white-text btn-flat"
           onClick={this.props.onCancel}
         >
           Back
+        </button>
+        <button className="green btn-flat white-text right">
+          Send Survey
+          <i className="material-icons right">email</i>
         </button>
       </div>
     );
